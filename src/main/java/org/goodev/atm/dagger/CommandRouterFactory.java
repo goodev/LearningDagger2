387139b8838ff2924a7de11bd8030f8fd0e1487f
yfcheng <yfcheng@mobvoi.com>
2020-01-07 20:14:26 +0800

    5. 使用多个 Module

--- src/main/java/org/goodev/atm/dagger/CommandRouterFactory.java
+++ src/main/java/org/goodev/atm/dagger/CommandRouterFactory.java
@@ -3,7 +3,7 @@ package org.goodev.atm.dagger;
 import dagger.Component;
 import org.goodev.atm.CommandRouter;
 
-@Component(modules = HelloWorldModule.class)
+@Component(modules = {HelloWorldModule.class, SystemOutModule.class})
 public interface CommandRouterFactory {
     CommandRouter router();
 }
