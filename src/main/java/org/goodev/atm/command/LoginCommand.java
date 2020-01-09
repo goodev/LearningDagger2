7b98d9e633e39ceab9cb07de6cd82745a671ada6
yfcheng <yfcheng@mobvoi.com>
2020-01-09 16:15:16 +0800

    19.使用 BindsOptionalOf 解决重复登录的问题

--- src/main/java/org/goodev/atm/command/LoginCommand.java
+++ src/main/java/org/goodev/atm/command/LoginCommand.java
@@ -5,22 +5,31 @@ import org.goodev.atm.Outputter;
 import org.goodev.atm.dagger.UserCommandsRouter;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 public class LoginCommand extends SingleArgCommand {
     private final Outputter mOutputter;
     private final Database mDatabase;
     private final UserCommandsRouter.Factory mFactory;
+    private final Optional<Database.Account> mAccount;
 
     @Inject
-    public LoginCommand(Outputter outputter, Database database, UserCommandsRouter.Factory factory) {
+    public LoginCommand(Outputter outputter, Database database, UserCommandsRouter.Factory factory,
+                        Optional<Database.Account> account) {
         System.err.println("创建 LoginCommand ：" + this);
         mOutputter = outputter;
         mDatabase = database;
         mFactory = factory;
+        mAccount = account;
     }
 
     @Override
     protected Result handleArg(String username) {
+        System.out.println("LoginCommand handleArg ：" + this);
+        if (mAccount.isPresent()) {
+            mOutputter.output(mAccount.get().username() + " 已经登录了。");
+            return Result.handled();
+        }
         Database.Account account = mDatabase.getAccount(username);
         mOutputter.output(username + " 已登录。账号余额为 " + account.balance());
         return Result.enterNestedCommandSet(mFactory.create(account).router());
--- src/main/java/org/goodev/atm/dagger/LoginCommandModule.java
+++ src/main/java/org/goodev/atm/dagger/LoginCommandModule.java
@@ -1,10 +1,12 @@
 package org.goodev.atm.dagger;
 
 import dagger.Binds;
+import dagger.BindsOptionalOf;
 import dagger.Module;
 import dagger.multibindings.IntoMap;
 import dagger.multibindings.StringKey;
 import org.goodev.atm.Command;
+import org.goodev.atm.Database;
 import org.goodev.atm.command.LoginCommand;
 
 @Module
@@ -13,4 +15,7 @@ public abstract class LoginCommandModule {
     @IntoMap
     @StringKey("登录")
     abstract Command loginCommand(LoginCommand command);
+
+    @BindsOptionalOf
+    abstract Database.Account optionalAccount();
 }
